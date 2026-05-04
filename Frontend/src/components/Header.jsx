414cf58b796cc24a04fb6cf7bfb04b2845ed67f3
José Manuel Mora Zúñiga <jose.morazuniga@ucr.ac.cr>
2026-05-04 10:39:24 -0600

    Header logos now link to home

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import ucrLogo from '../assets/firma-tipografica-una-linea-blanco.png'
 import vraLogo from '../assets/vrasgv/VRA_B-N_2.svg'
 
@@ -15,16 +16,20 @@ export default function Header() {
         gap: '20px',
       }}
     >
-      <img
-        src={ucrLogo}
-        alt="Universidad de Costa Rica"
-        style={{ height: '55px', objectFit: 'contain' }}
-      />
-      <img
-        src={vraLogo}
-        alt="Vicerrectoría de Administración"
-        style={{ height: '75px', objectFit: 'contain', maxWidth: '220px' }}
-      />
+      <Link to="/home" style={{ display: 'inline-flex' }}>
+        <img
+          src={ucrLogo}
+          alt="Universidad de Costa Rica"
+          style={{ height: '55px', objectFit: 'contain' }}
+        />
+      </Link>
+      <Link to="/home" style={{ display: 'inline-flex' }}>
+        <img
+          src={vraLogo}
+          alt="Vicerrectoría de Administración"
+          style={{ height: '75px', objectFit: 'contain', maxWidth: '220px' }}
+        />
+      </Link>
     </header>
   )
 }
