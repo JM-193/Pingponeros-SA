2ad2ca6201a9ca451d3fe1d44113fbd9e4a7267c
Sebastian Gomez <sebastian.gomezcastro@ucr.ac.cr>
2026-05-03 20:59:49 -0600

    Update ESLint configuration and standardize font family in CSS

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -26,5 +26,9 @@ export default defineConfig([
     settings: {
       react: { version: 'detect' },
     },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+    },
   },
 ])
