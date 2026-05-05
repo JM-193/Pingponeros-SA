dce0e1641f24f802683d9d16cf6944015382d7b0
Carlos Obando <carlos.obandolopez@ucr.ac.cr>
2026-05-04 19:51:28 -0600

    Add route for CreateUser page in the router

--- Frontend/src/router/index.jsx
+++ Frontend/src/router/index.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from '../pages/Login'
 import Home from '../pages/Home'
 import ForgotPassword from '../pages/ForgotPassword'
+import CreateUser from '../pages/CreateUser'
 
 export default function AppRouter() {
   return (
@@ -10,6 +11,7 @@ export default function AppRouter() {
         <Route path="/" element={<Login />} />
         <Route path="/recuperar-contrasena" element={<ForgotPassword />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/usuarios/crear" element={<CreateUser />} />
       </Routes>
     </BrowserRouter>
   )
