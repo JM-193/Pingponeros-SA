3530dcebfa33696f7cad39682e173e36af7551b5
José Manuel Mora Zúñiga <jose.morazuniga@ucr.ac.cr>
2026-05-05 22:52:48 -0600

    Lint

--- Frontend/src/pages/CreateArea.jsx
+++ Frontend/src/pages/CreateArea.jsx
@@ -5,7 +5,6 @@ import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import FormContainer from '../components/FormContainer'
-import FormInput from '../components/FormInput'
 import FormButton from '../components/FormButton'
 import { COLORS } from '../constants/colors'
 
