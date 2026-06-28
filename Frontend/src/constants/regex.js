796b8d029ab9e049f63833e4973586d4407ae5b2
Jose Manuel Mora Z <jose.morazuniga@ucr.ac.cr>
2026-06-27 21:38:59 -0600

    Fix SonarQube error - Was whining about a possible RegEx being unoptimized

--- Frontend/src/constants/regex.js
+++ Frontend/src/constants/regex.js
@@ -4,4 +4,4 @@
 export const EMAIL_REGEX = /^[a-zA-Z]+\.[a-zA-Z]+@[uU][cC][rR]\.[aA][cC]\.[cC][rR]$/
 
 // Mirrors ValidationPatterns.SoloLetras (Backend): letters only, optional surrounding whitespace.
-export const SOLO_LETRAS_REGEX = /^\s*([A-Za-záéíóúÁÉÍÓÚñÑüÜ]+)?\s*$/
+export const SOLO_LETRAS_REGEX = /^(?:\s+|\s*[A-Za-záéíóúÁÉÍÓÚñÑüÜ]+\s*)$/
